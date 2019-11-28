b0c0dbd2fd66e2acbb30cfb6bc533853326eb6cc
mike <mike@mmaher-dell-lap.localdomain>
2019-11-28 12:23:50 +0000

    [UPDATE] added get single client from route params

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { ClientService } from '../../services/client.service';
+import Client from '../../models/Client';
 
 @Component({
   selector: 'app-client-details',
   templateUrl: './client-details.component.html',
-  styleUrls: ['./client-details.component.css']
+  styleUrls: ['./client-details.component.css'],
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceEditForm: boolean = false;
 
-  constructor() { }
+  constructor(
+    private service: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService,
+  ) {}
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.service.getClient(this.id).subscribe(client => {
+      if (client) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      this.client = client;
+    });
   }
-
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -28,6 +28,22 @@ export class ClientService {
     this.clientsCollection.add(client);
   }
 
+  getClient(id: string): Observable<Client> {
+    this.clientDocument = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDocument.snapshotChanges().pipe(
+      map(action => {
+        if (action.payload.exists === false) {
+          return null;
+        } else {
+          const data = action.payload.data() as Client;
+          data.id = action.payload.id;
+          return data;
+        }
+      }),
+    );
+    return this.client;
+  }
+
   getClients(): Observable<Client[]> {
     this.clients = this.clientsCollection.snapshotChanges().pipe(
       map(actions =>
